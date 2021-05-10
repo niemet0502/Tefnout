2abc9fd551050812fcfd5ff70c602a346f63893c
niemet0502 <vincentmarius8@gmail.com>
2021-05-10 16:03:01 +0200

    api :: added relationship method on Quizz model

--- api/app/Models/Quizz.php
+++ api/app/Models/Quizz.php
@@ -9,4 +9,20 @@ class Quizz extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'description', 'section_id'];
+
+    /**
+     * Get the setion that owns the quizz.
+     */
+    public function section()
+    {
+        return $this->belongsTo(Section::class, 'foreign_key');
+    }
+
+     /**
+     * Get the questions for the quizz.
+     */
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
 }
