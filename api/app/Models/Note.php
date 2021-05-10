68c540898c0842989a5aac7c9ae7f46c820973a7
niemet0502 <vincentmarius8@gmail.com>
2021-05-10 16:02:22 +0200

    api :: added relationship method on Note model

--- api/app/Models/Note.php
+++ api/app/Models/Note.php
@@ -9,4 +9,20 @@ class Note extends Model
 {
     use HasFactory;
     protected $fillable = ['user_id', 'course_id', 'value'];
+
+    /**
+     * Get the course that owns the note.
+     */
+    public function course()
+    {
+        return $this->belongsTo(Course::class, 'foreign_key');
+    }
+
+    /**
+     * Get the user that owns the note.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'foreign_key');
+    }
 }
