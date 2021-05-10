85b0c4830af4a5b0857567227f4496717fe57084
niemet0502 <vincentmarius8@gmail.com>
2021-05-10 16:02:00 +0200

    api :: added relationship method on Comments model

--- api/app/Models/Comments.php
+++ api/app/Models/Comments.php
@@ -9,4 +9,20 @@ class Comments extends Model
 {
     use HasFactory;
     protected $fillable = ['user_id', 'course_id', 'content'];
+
+     /**
+     * Get the course that owns the note.
+     */
+    public function course()
+    {
+        return $this->belongsTo(Course::class, 'foreign_key');
+    }
+
+    /**
+     * Get the user that owns the note.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'foreign_key');
+    }
 }
