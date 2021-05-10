e658088f5f7b1eb03cede9cc6f69f7fcc142b71c
niemet0502 <vincentmarius8@gmail.com>
2021-05-10 16:02:51 +0200

    api :: added relationship method on Question model

--- api/app/Models/Question.php
+++ api/app/Models/Question.php
@@ -9,4 +9,20 @@ class Question extends Model
 {
     use HasFactory;
     protected $fillable = ['label', 'description'];
+
+    /**
+     * Get the quizz that owns the question.
+     */
+    public function quizz()
+    {
+        return $this->belongsTo(Quizz::class, 'foreign_key');
+    }
+
+     /**
+     * Get the answers for the question.
+     */
+    public function answers()
+    {
+        return $this->hasMany(Answers::class);
+    }
 }
