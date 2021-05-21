ddef85da4c5833e222c911a79f5eff1b7a04505b
niemet0502 <vincentmarius8@gmail.com>
2021-05-21 21:56:13 +0200

    api :: added few field on user table

--- api/database/migrations/2014_10_12_000000_create_users_table.php
+++ api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('firstname');
             $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('avatar');
+            $table->string('function');
+            $table->string('bio');
+            $table->string('siteweb');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
