98abf9e321a8d65b3cf768ecf41f16997c08d3fc
niemet0502 <vincentmarius8@gmail.com>
2021-05-22 13:41:00 +0200

    api :: create Quizz model

---
+++ api/app/Models/Quizz.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Quizz extends Model
+{
+    use HasFactory;
+}
