b672cae5db0ecb536a18d76b2ab0546e742fc31c
niemet0502 <vincentmarius8@gmail.com>
2021-05-22 13:36:20 +0200

    api :: create chapters table on database

---
+++ api/database/migrations/2021_05_22_113404_create_chapters_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChaptersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chapters', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->longText('textContent');
+            $table->string('video');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chapters');
+    }
+}
