f18d07bbdd005389f02193884c612e914a71cb7c
niemet0502 <vincentmarius8@gmail.com>
2021-05-23 15:51:34 +0200

    api :: used HasApiTokens on Question model

--- api/app/Models/Question.php
+++ api/app/Models/Question.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
 
 class Question extends Model
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens;
     protected $fillable = ['label', 'description'];
 
     /**
