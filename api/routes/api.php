31512026bdbec8cff0a18645a92d9fe5650599ca
niemet0502 <vincentmarius8@gmail.com>
2021-05-23 14:47:39 +0200

    api :: created user's routes

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/users', function(){
+    return User::all();
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
