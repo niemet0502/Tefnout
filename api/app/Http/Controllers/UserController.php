033737bbd9daf45135afdf068fa5a7e9b8b7c8ce
niemet0502 <vincentmarius8@gmail.com>
2021-05-23 15:45:18 +0200

    api :: retrieve user's list

--- api/app/Http/Controllers/UserController.php
+++ api/app/Http/Controllers/UserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
 class UserController extends Controller
 {
     /**
@@ -13,7 +13,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        return User::all();
     }
 
     /**
