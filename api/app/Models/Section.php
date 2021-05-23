32b6dc6a791f2aff4cc87adcedaadff5afb88442
niemet0502 <vincentmarius8@gmail.com>
2021-05-23 15:52:00 +0200

    api :: used HasApiTokens on Section model

--- api/app/Models/Section.php
+++ api/app/Models/Section.php
@@ -4,10 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Laravel\Sanctum\HasApiTokens;
 class Section extends Model
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens;
     protected $fillable = ['title', 'description', 'course_id'];
     
      /**
