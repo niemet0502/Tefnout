7b1bfe56e9f2c23f10bdcdd7f4298086d7b14bac
niemet0502 <vincentmarius8@gmail.com>
2021-05-23 15:45:41 +0200

    api :: used sanctum in model

--- api/app/Models/Course.php
+++ api/app/Models/Course.php
@@ -4,10 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Laravel\Sanctum\HasApiTokens;
 class Course extends Model
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens;
     protected $fillable = [
         'teacher_id',
         'category_id',
