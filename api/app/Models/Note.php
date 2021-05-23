9be83100bd6e09d43cbad9b54488a7978ea0131e
niemet0502 <vincentmarius8@gmail.com>
2021-05-23 15:51:13 +0200

    api :: used HasApiTokens on Note model

--- api/app/Models/Note.php
+++ api/app/Models/Note.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
 
 class Note extends Model
 {
-    use HasFactory;
+    use HasFactory,HasApiTokens;
     protected $fillable = ['user_id', 'course_id', 'value'];
 
     /**
