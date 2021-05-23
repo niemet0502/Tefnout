ae305be1001082f49222c9db6de88aa5d5e64aa5
niemet0502 <vincentmarius8@gmail.com>
2021-05-23 15:02:47 +0200

    api :: make users fields nullable

--- api/database/migrations/2014_10_12_000000_create_users_table.php
+++ api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,13 +16,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('firstname');
+            $table->string('firstname')->nullable()->default(null);
             $table->string('email')->unique();
-            $table->string('phone');
-            $table->string('avatar');
-            $table->string('function');
-            $table->string('bio');
-            $table->string('siteweb');
+            $table->string('phone')->nullable()->default(null);
+            $table->string('avatar')->nullable()->default(null);
+            $table->string('function')->nullable()->default(null);
+            $table->string('bio')->nullable()->default(null);
+            $table->string('siteweb')->nullable()->default(null);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
