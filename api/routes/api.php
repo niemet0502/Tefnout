750a58c5870232291545bf5aaec144133675ae2b
niemet0502 <vincentmarius8@gmail.com>
2021-05-24 14:35:54 +0200

    api :: create routes for category models

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
@@ -22,6 +23,10 @@ Route::post('/login', [AuthController::class, 'login']);
 
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    // Category's routes
+    Route::resource('categories', CategoryController::class);
+
     Route::get('/users', [UserController::class, 'index']);
     Route::get('/courses', [CourseController::class, 'index']);
     Route::post('/logout', [AuthController::class, 'logout']);
