9b1e2dc0bf3de2103970e647b6ddcd20d0daeaa8
niemet0502 <vincentmarius8@gmail.com>
2021-06-17 15:36:54 +0200

    client :: clean up

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
+import Topbar from "./components/Marketplace/Topbar"
+import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
 function App() {
   return (
     <div className="App">
-     Marius
+     <Topbar />
     </div>
   );
 }
