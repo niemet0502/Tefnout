24e44dcdf963cb2cc4368eb32fda31dff99b3f3b
niemet0502 <vincentmarius8@gmail.com>
2021-06-23 17:10:58 +0200

    client :: upload assets

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
 // eslint-disable-next-line
-import Topbar from "./components/Marketplace/Topbar"
 import "./App.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
-import Instructor from "./components/Marketplace/Instructor"
+import Home from "./pages/Home"
 
 function App() {
   return (
     <div className="App">
       <div className="row justify-content-md-center">
-        <Instructor/>
+        <Home />
       </div>
     </div>
   );
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import hero_icon from "../assets/img/hero_icon.png"
+import hero_img from "../assets/img/hero_img.png"
+import styled from "styled-components"
+function Home() {
+  return (
+    <HomePage>
+
+    <div>
+      <section class="hero_area">
+                <div class="hero_wrap hero_height">
+                    <div class="container-fluid">
+                        <div class="row flex-row-reverse align-items-center">
+                            <div class="col-xl-7 col-lg-6 col-md-12">
+                                <div class="hero_img f-right">
+                                    <img src={hero_img} alt=""/>
+                                </div>
+                            </div>
+                            <div class="col-xl-5 col-lg-6 col-md-12">
+                                <div class="hero_content">
+                                    <div class="icon">
+                                        <img src={hero_icon} alt=""/>
+                                    </div>
+                                    <span># Learn Anything, Anytime, Anywhere</span>
+                                    <h2>Best Online Learning For your future.</h2>
+                                    <p>We believe everyone has the capacity to be creative. Turitor is a place where
+                                        people develop their own potential.</p>
+                                    <div class="hero_btn mt-15">
+                                        <a class="thm_btn" href="courses.html">View Courses<i class="fal fa-long-arrow-right"></i></a>
+                                        <a class="thm_btn thm_btn-2" href="about.html">Start Trial<i class="fal fa-long-arrow-right"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+    </div>
+    </HomePage>
+
+  )
+}
+
+const HomePage = styled.div`
+  margin: 0;
+  padding: 0;
+
+  .hero_area {
+    background: #fff;
+  }
+  .hero_wrap {
+  padding-left: 140px;
+	padding-right: 140px;
+	display: flex;
+	align-items: center;
+}
+.hero_content .icon {
+	margin-bottom: 36px;
+}
+.hero_content span {
+	font-size: 18px;
+	font-weight: 600;
+	display: inline-block;
+	color: #e5175c;
+	margin-bottom: 20px;
+}
+.hero_content h2 {
+	font-size: 60px;
+	line-height: 70px;
+	margin-bottom: 30px;
+	max-width: 700px;
+}
+.hero_content p {
+	font-size: 18px;
+	line-height: 29px;
+	max-width: 700px;
+}
+.hero_content .hero_btn a {
+	margin-top: 20px;
+	margin-right: 20px;
+}
+.hero_content .hero_btn a:last-child {
+    margin-right: 0;
+}
+`;
+export default Home
