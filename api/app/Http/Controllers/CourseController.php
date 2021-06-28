838cafe48a9c94f471f9f01f01000232f8d9b500
niemet0502 <vincentmarius8@gmail.com>
2021-06-28 17:46:34 +0200

    api :: created searchCourse feature

--- api/app/Http/Controllers/CourseController.php
+++ api/app/Http/Controllers/CourseController.php
@@ -110,4 +110,19 @@ class CourseController extends Controller
 
         return $response;
     }
+
+    public function searchCourse($name){
+        return Course::select('courses.*', 
+            'users.avatar as teacher_image',
+            'users.name as teacher_nae', 
+            'categories.name as category_name')
+            ->join('users', 'users.id', '=', 'courses.teacher_id')
+            ->join('categories', 'categories.id', '=', 'courses.category_id' )
+            ->where('title', 'like', '%'.$name.'%')
+            ->orWhere('description', 'like', '%'.$name.'%') 
+            ->orWhere('topics', 'like', '%'.$name.'%') 
+            ->orWhere('categories.name', 'like', '%'.$name.'%') 
+            ->orWhere('users.name', 'like', '%'.$name.'%')   
+            ->get();
+    }
 }
