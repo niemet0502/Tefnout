969e5d88d191f95df60c87efd7254ba762695930
niemet0502 <vincentmarius8@gmail.com>
2021-07-13 12:34:59 +0200

    client :: build ButtonComponent

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,29 @@ import "./App.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
 import Home from "./pages/Home"
+import Button from "./components/Marketplace/Button"
+
+
 function App() {
   return (
-    <div className="App">
-      <Home/>
+    <div className="App" style={{background: '#f7f7f7'}}>
+      <Button 
+        text="Views courses" 
+        bgColor="#e5175c"
+        bgColorHover="#0073ff" />
+
+      <Button 
+        text="Views courses"  />
+
+      <Button 
+        text="ALL CATEGORIES" 
+        bgColor="#0073ff"
+        bgColorHover="#0073ff" />
+
+      <Button 
+        text="DATA SCIENCE" 
+        bgColor="#ffff"
+        bgColorHover="#0073ff"/>
     </div>
   );
 }
--- client/src/components/Marketplace/Button.jsx
+++ client/src/components/Marketplace/Button.jsx
@@ -1,11 +1,46 @@
 import React from 'react'
+import styled from 'styled-components'
 
-function Button() {
+// icons 
+import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt';
+function Button({
+  variant, 
+  text, 
+  Icon,
+  bgColor,
+  bgColorHover,}) {
   return (
-    <div>
-      
-    </div>
+    <ButtonComponent bgColor={bgColor} bgColorHover={bgColorHover}>
+      {text} <ArrowRightAltIcon />
+    </ButtonComponent>
   )
 }
+
+const ButtonComponent = styled.button`
+  
+  margin: 5px;
+	font-size: 14px;
+	padding: 16px 30px;
+	text-align: center;
+	position: relative;
+	color: ${props => props.bgColor == '#ffff' ? '#8d8d8d' : '#ffff'};
+	text-transform: uppercase;
+	font-weight: 600;
+	display: inline-block;
+	border: 2px solid transparent;
+  background-color: ${props => props.bgColor || "#0073ff"};
+
+  display: flex;
+  align-items: center;
+
+  &:hover {
+    color: #ffff;
+    background-color: ${props => props.bgColorHover || "#e5175c"};
+  }
+
+  svg{
+    margin-left: 5px;
+  }
+`;
 
 export default Button
