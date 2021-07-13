5ccfea944488e3742a945031f392f4b8f3b437d1
niemet0502 <vincentmarius8@gmail.com>
2021-07-13 14:54:46 +0200

    client :: imported and used react-router-dom for route's creation

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,29 @@
 // eslint-disable-next-line
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+// style 
 import "./App.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
-import Home from "./pages/Home"
-import Button from "./components/Marketplace/Button"
 
+// components 
+import Home from "./pages/Home"
+import Topbar from "./components/Marketplace/Topbar";
+import Courses from "./pages/Courses";
 
 function App() {
   return (
-    <div className="App" style={{background: '#f7f7f7'}}>
-      <Home/>
-    </div>
+    <Router>
+      <Topbar />
+      <Switch>
+        <Route  component={Home} path="/" exact />
+        <Route  component={Courses} path="/courses" exact />
+      </Switch>
+  </Router>
   );
 }
 
