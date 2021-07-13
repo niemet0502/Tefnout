bf789b612c6d90ae0e19c6acbeb5e0e17e96fa86
niemet0502 <vincentmarius8@gmail.com>
2021-07-13 11:19:00 +0200

    client :: created ButtonComponent struture

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,10 @@ import "./App.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
 import Home from "./pages/Home"
-import CourseCard from "./components/Marketplace/CourseCard"
 function App() {
   return (
     <div className="App">
-      <div className="row justify-content-md-center" style={{backgroundColor: '#f7f7f7'}}>
-        <h3>The Nothing</h3>
-        <Home/>
-      </div>
+      <Home/>
     </div>
   );
 }
