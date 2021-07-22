f39fd469e18c08fa7aa9e0bfcc0b2877ccda51dc
niemet0502 <vincentmarius8@gmail.com>
2021-07-22 20:31:07 +0200

    client :: used ScrollToTop component in BrowserRouter

--- client/src/App.js
+++ client/src/App.js
@@ -13,17 +13,22 @@ import "bootstrap/dist/js/bootstrap.min.js"
 // components 
 import Home from "./pages/Home"
 import Topbar from "./components/Marketplace/Topbar";
+import ScrollToTop from "./components/ScrollToTop";
+
+//pages 
 import Courses from "./pages/Courses";
 import CourseDetails from "./pages/CourseDetails";
-
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Topbar />
       <Switch>
         <Route  component={Home} path="/" exact />
         <Route  component={Courses} path="/courses" exact />
+        <Route  component={Login} path="/login" exact />
         <Route  component={CourseDetails} path="/course-details" exact />
       </Switch>
   </Router>
