a0b5cd9955161e83f2d0f83f9af0f59d74c8fde9
niemet0502 <vincentmarius8@gmail.com>
2021-08-08 15:30:55 +0200

    admin :: eslint config file

---
+++ admin-app/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+      'jest/globals': true,
+    },
+    extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    plugins: ['react', 'jest'],
+    rules: {},
+    overrides: [
+      {
+        files: ['*.jsx', '*.js'],
+      },
+    ],
+  };
