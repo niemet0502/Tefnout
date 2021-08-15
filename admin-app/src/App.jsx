14c4050ae854fb5fa164fbf8792593e9a6ffbab7
niemet0502 <vincentmarius8@gmail.com>
2021-08-15 02:00:09 +0200

    admin :: displayed logo in TopBar

--- admin-app/src/App.jsx
+++ admin-app/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
+
+// style
 import './App.css';
-
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
+import Sidebar from './components/common/Sidebar/Sidebar';
+import TopBar from "./components/common/TopBar"
 function App() {
   return (
     <div className="App">
-      
+     <TopBar />
     </div>
   );
 }
