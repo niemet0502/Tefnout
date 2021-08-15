f03f4a768e8012d482e9ce07d5decb2bef40fa3a
niemet0502 <vincentmarius8@gmail.com>
2021-08-15 05:13:15 +0200

    admin :: used ProTypes for validate type of Card's props

--- admin-app/src/components/common/Card.jsx
+++ admin-app/src/components/common/Card.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import PropTypes from 'prop-types';
+
 import achievement from "../../assets/img/achievement.svg"
-function Card() {
+
+function Card({title,value,image}) {
   return (
     <div className="card_dash d-flex align-items-center justify-content-between">
       <div className="card_dash_left">
-        <h5>Total Sales</h5>
-        <h2>$350</h2>
-        <span className="crdbg_1">New $50</span>
+        <h5>{title}</h5>
+        <h2>{value}</h2>
       </div>
       <div className="card_dash_right">
         <img src={achievement} alt="achievement" />
@@ -14,5 +16,11 @@ function Card() {
     </div>
   )
 }
+
+Card.propTypes = {
+  image: PropTypes.string.isRequired,
+  value: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+};
 
 export default Card
