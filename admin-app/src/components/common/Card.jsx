5c4574efb71faafdc9bd587602de52fdd4f2d89e
niemet0502 <vincentmarius8@gmail.com>
2021-08-15 04:58:55 +0200

    admin :: created card component

--- admin-app/src/components/common/Card.jsx
+++ admin-app/src/components/common/Card.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import achievement from "../../assets/img/achievement.svg"
 function Card() {
   return (
-    <div>
-      
+    <div className="card_dash d-flex align-items-center justify-content-between">
+      <div className="card_dash_left">
+        <h5>Total Sales</h5>
+        <h2>$350</h2>
+        <span className="crdbg_1">New $50</span>
+      </div>
+      <div className="card_dash_right">
+        <img src={achievement} alt="achievement" />
+      </div>
     </div>
   )
 }
