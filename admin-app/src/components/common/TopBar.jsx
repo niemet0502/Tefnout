1ee410d736aa6dcd187ce494d3e06c3ff8d80b27
niemet0502 <vincentmarius8@gmail.com>
2021-08-15 03:00:25 +0200

    admin :: adde user's dropdown in Topbar

--- admin-app/src/components/common/TopBar.jsx
+++ admin-app/src/components/common/TopBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 //images 
 import user_profil from "../../assets/img/user_profil.jpg";
@@ -10,8 +10,15 @@ import MailOutlineOutlinedIcon from '@material-ui/icons/MailOutlineOutlined';
 import NotificationsActiveOutlinedIcon from '@material-ui/icons/NotificationsActiveOutlined';
 import MenuOutlinedIcon from '@material-ui/icons/MenuOutlined';
 import DashboardOutlinedIcon from '@material-ui/icons/DashboardOutlined';
-import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import NightsStayOutlinedIcon from '@material-ui/icons/NightsStayOutlined';
+
 function TopBar() {
+	const [userDropdown, setUserDropdown] = useState(false)
+
+	const toggleUserDropdown = () => {
+		setUserDropdown(!userDropdown);
+	}
+
   return (
     <header className="header clearfix">
 		<button id="collapse_menu" className="collapse_menu d-flex align-items-center justify-content-center">
@@ -53,10 +60,40 @@ function TopBar() {
 					 
 				</li>
 				<li className="ui dropdown">
-					<a href="#" className="opts_account" title="Account">
-					<img src={user_profil} alt="user_profil" />
+					<a href="#" className="opts_account" title="Account" onClick={toggleUserDropdown}>
+						<img src={user_profil} alt="user_profil" />
 					</a>
+					{userDropdown && 
 					
+					<div className="menu dropdown_account transition">
+						<div className="channel_my">
+							<div className="profile_link">
+							<img src={user_profil} alt="user_profil" />
+								<div className="pd_content">
+									<div className="rhte85">
+										<h6>Joginder Singh</h6>
+										<div className="mef78" title="Verify">
+											<i className='uil uil-check-circle'></i>
+										</div>
+									</div>
+									<span>gambol943@gmail.com</span>
+								</div>							
+							</div>
+							<a href="my_instructor_profile_view.html" className="dp_link_12">View Instructor Profile</a>						
+						</div>
+						<div className="night_mode_switch__btn">
+							<a href="#" id="night-mode" className="btn-night-mode">
+								<span className="icon"><NightsStayOutlinedIcon /></span> Night mode
+								<span className="btn-night-mode-switch">
+									<span className="uk-switch-button"></span>
+								</span>
+							</a>
+						</div>
+						<a href="instructor_dashboard.html" className="item channel_item">Cursus dashboard</a>						
+						<a href="sign_in.html" className="item channel_item">Sign Out</a>
+					</div>
+					}
+
 				</li>
 			</ul>
 		</div>
