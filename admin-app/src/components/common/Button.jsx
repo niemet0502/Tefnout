f69e38fd2b79e9f915d1118b3062b918a235e692
niemet0502 <vincentmarius8@gmail.com>
2021-08-18 15:17:15 +0200

    admin :: created Button component

--- admin-app/src/components/common/Button.jsx
+++ admin-app/src/components/common/Button.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import styled from "styled-components";
+import PropTypes from 'prop-types'; 
 
-function Button() {
+function Button({text}) {
   return (
-    <div>
-      
-    </div>
+    <ButtonComponent>
+      {text}
+    </ButtonComponent>
   )
 }
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired
+}
+
+const ButtonComponent = styled.button`
+  font-size: 14px;
+  font-weight: 500;
+  font-family: 'Roboto', sans-serif;
+  color: #fff;
+  background: #ed2a26;
+  padding: 9px 15px;
+  border-radius: 3px;
+  border: 1px solid #ed2a26;
+`;
 
 export default Button
