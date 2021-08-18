e49e2d211c48b45296270ca85c885ff44c944211
niemet0502 <vincentmarius8@gmail.com>
2021-08-18 14:53:35 +0200

    admin :: init Row component

--- admin-app/src/components/common/Table/Row.jsx
+++ admin-app/src/components/common/Table/Row.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import PropTypes from 'prop-types';
+
+//components 
+import Cell from './Cell';
 
 function Row({data}) {
   return (
-    <div>
-      
-    </div>
+    <tr>
+      {data.map((cell, index) => {
+       <h1>Test</h1>
+      })}
+    </tr>
   )
 }
 
