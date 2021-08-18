589b132ecf9bb1179cd40272c4df6b5f8d6fc006
niemet0502 <vincentmarius8@gmail.com>
2021-08-18 13:35:40 +0200

    admin :: renamed TableItem component to Cell component

--- admin-app/src/components/common/Table/Cell.jsx
+++ admin-app/src/components/common/Table/Cell.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function TableItem() {
+function Cell() {
   return (
     <div>
       
@@ -8,4 +8,4 @@ function TableItem() {
   )
 }
 
-export default TableItem
+export default Cell
