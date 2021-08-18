5740eeade48f41ab8563dfdb5ec2fc6e1f4db8a3
niemet0502 <vincentmarius8@gmail.com>
2021-08-18 15:26:24 +0200

    admin :: init Footer component

--- admin-app/src/App.jsx
+++ admin-app/src/App.jsx
@@ -5,17 +5,21 @@ import {
   Route,
 } from "react-router-dom";
 
-
 // style
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
+
+// components
 import Sidebar from './components/common/Sidebar/Sidebar';
 import TopBar from "./components/common/TopBar"
+import Dashboard from './pages/Dashboard';
 function App() {
   return (
     <div className="App">
      <TopBar />
+     <Sidebar />
+     <Dashboard />
     </div>
   );
 }
--- admin-app/src/pages/Dashboard.jsx
+++ admin-app/src/pages/Dashboard.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import Card from '../components/common/Card'
+import Button from '../components/common/Button'
 function Dashboard() {
   return (
-    <div>
+    <div className="wrapper">
+      <div className="container-fluid">
+      <div className="row d-flex">
+        <div className="col-md-6"><Card title="Total issue" value={15} /></div>
+        <div className="col-md-6"><Card title="Total issue" value={15} /></div>
+      </div>
+
+
+      <Button />
       
+
+      <table className="mt-5" style={{height: '4000px', border: '1px solid blue'}}>
+        <tr>
+          <td>marius</td>
+        </tr>
+      </table>
+      </div>
     </div>
   )
 }
