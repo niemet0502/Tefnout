43d7d634af1c0ad15f9a0b812a5599f70a0c44d1
niemet0502 <vincentmarius8@gmail.com>
2021-08-23 15:45:43 +0200

    student :: fix className issue on CourseDetail component

--- student-app/src/pages/CourseDetails.jsx
+++ student-app/src/pages/CourseDetails.jsx
@@ -24,36 +24,36 @@ function CourseDetails() {
       page="DETAILS" />
 
 
-      <section class="course_details_area pt-5 pb-5">
-        <div class="container mt-5">
-          <div class="row">
-            <div class="col-lg-8">
-              <div class="course_dtls_left mb-30 p-3 pb-4" style={{background: '#ffff'}}> 
-                <div class="cd_thumb">
+      <section className="course_details_area pt-5 pb-5">
+        <div className="container mt-5">
+          <div className="row">
+            <div className="col-lg-8">
+              <div className="course_dtls_left mb-30 p-3 pb-4" style={{background: '#ffff'}}> 
+                <div className="cd_thumb">
                   <img src={cd_thumb} alt="" style={{width: '100%'}} />
                 </div>
-                <div class="cd_content">
-                    <div class="meta d-flex">
-                        <div class="left ul_li d-flex">
-                            <ul class="rating_star ul_li d-flex pl-0">
+                <div className="cd_content">
+                    <div className="meta d-flex">
+                        <div className="left ul_li d-flex">
+                            <ul className="rating_star ul_li d-flex pl-0">
                                 <li><StarIcon /></li>
                                 <li><StarIcon /></li>
                                 <li><StarIcon /></li>
                                 <li><StarBorderIcon /></li>
                                 <li><StarBorderIcon /></li>
                             </ul>
-                            <div class="review">
+                            <div className="review">
                                 <span>132 Reviews</span>
                             </div>
                         </div>
-                        <div class="right ul_li">
-                            <span class="ptice">Debutant</span>
+                        <div className="right ul_li">
+                            <span className="ptice">Debutant</span>
                         </div>
                     </div>
-                    <h3 class="title">The Complete ui/ux design course for beginner.</h3>
-                    <div class="bottom">
-                        <div class="left ul_li d-flex align-items-center">
-                            <div class="author">
+                    <h3 className="title">The Complete ui/ux design course for beginner.</h3>
+                    <div className="bottom">
+                        <div className="left ul_li d-flex align-items-center">
+                            <div className="author">
                                 <img src="" alt="" />
                             </div>
                             <h4><span>By :</span>Rasalina Willam</h4>
@@ -61,7 +61,7 @@ function CourseDetails() {
                         
                     </div>
                 </div>
-              <div class="cdl_bottom"> 
+              <div className="cdl_bottom"> 
               <div>
                 <h1>Tabs Demo</h1>
                 <Tabs> 
@@ -81,43 +81,43 @@ function CourseDetails() {
             </div>
 
             {/* sidebar */}
-            <div class="col-lg-4">
-            <div class="course_dtls_right">
-              <div class="course_widget mb-30">
-                  <div class="thumb_wrap pos-rel">
-                      <div class="thumb">
+            <div className="col-lg-4">
+            <div className="course_dtls_right">
+              <div className="course_widget mb-30">
+                  <div className="thumb_wrap pos-rel">
+                      <div className="thumb">
                           <img src={cd_thumb} alt="" />
                       </div>
                      
                   </div>
-                  <div class="course_content">
+                  <div className="course_content">
                       <ul>
                           <li>
-                              <span class="left">Duration :</span>
+                              <span className="left">Duration :</span>
                               <span>3 Hours</span>
                           </li>
                           <li>
-                              <span class="left">Lectures :</span>
+                              <span className="left">Lectures :</span>
                               <span>148</span>
                           </li>
                           <li>
-                              <span class="left">Skill Level :</span>
+                              <span className="left">Skill Level :</span>
                               <span>Beginner</span>
                           </li>
                           <li>
-                              <span class="left">Quizzers :</span>
+                              <span className="left">Quizzers :</span>
                               <span>09</span>
                           </li>
                           <li>
-                              <span class="left">Pass Pareentages</span>
+                              <span className="left">Pass Pareentages</span>
                               <span>80</span>
                           </li>
                           <li>
-                              <span class="left">Certificate</span>
+                              <span className="left">Certificate</span>
                               <span>Yes</span>
                           </li>
                       </ul>
-                      <div class="course_btn text-center mt-3 d-flex justify-content-center">
+                      <div className="course_btn text-center mt-3 d-flex justify-content-center">
                         <Button 
                           text="Enroll Course"
                           bgColorHover="#0073ff"
