b1990e56dbe01126264fb7ff69357fc259fbb92b
niemet0502 <vincentmarius8@gmail.com>
2021-08-23 15:46:17 +0200

    student :: updated eslint config

--- student-app/.eslintrc.js
+++ student-app/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    "extends": ['plugin:react/recommended', 'airbnb'],
+    "extends": ['plugin:react/recommended'],
     "parserOptions": {
         "ecmaFeatures": {
             "jsx": true
