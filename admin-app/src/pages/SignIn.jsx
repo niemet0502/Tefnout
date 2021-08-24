62342189d83de03084c27981683edf5da8244d48
niemet0502 <vincentmarius8@gmail.com>
2021-08-24 18:19:51 +0200

    admin :: used FormInput and Button component in SignIn page

--- admin-app/src/pages/SignIn.jsx
+++ admin-app/src/pages/SignIn.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from "styled-components"
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import VpnKeyIcon from '@material-ui/icons/VpnKey';
 
 // components 
 import Button from '../components/common/Button';
+import FormInput from '../components/form/FormInput';
 
 import logo from "../assets/img/logo.svg"
 
 function SignIn() {
+	const [user, setUser] = useState({username: "", password: ""})
+	const [errors, setError] = useState({})
 
-	function handleClick(){
-		console.log('marius');
-	}
+	function handleChange(event){
+    let user = {username: "",password: ""}
+    user[event.target.name] = event.target.value;
+    setUser(user)
+  };
 
   return (
     <div className="sign_in_up_bg">
@@ -33,22 +38,40 @@ function SignIn() {
 						<form>
 							<div className="ui search focus mt-2">
 								<div className="ui left icon input swdh95">
-									<input className="prompt srch_explore" type="email" name="emailaddress" value="" id="id_email" required="" placeholder="Email Address"/>															
+									<FormInput
+									name="email"
+									type="email"
+									value={user.password}
+									onChange={handleChange}
+									placeholder="Enter Address..."
+									className="prompt srch_explore"
+									error={errors.password}
+									required
+								/>
 									<MailOutlineIcon  className="input__icon"/>
 								</div>
 							</div>
 							<div className="ui search focus mt-2">
 								<div className="ui left icon input swdh95">
-									<input className="prompt srch_explore" type="password" name="password" value="" id="id_password" required=""  placeholder="Password"/>
+								<FormInput
+									name="password"
+									type="password"
+									value={user.password}
+									onChange={handleChange}
+									placeholder="Enter password..."
+									className="prompt srch_explore"
+									error={errors.password}
+									required
+								/>
 									<VpnKeyIcon className="input__icon" />
 								</div>
 							</div>
-							 <Button text="SignIn" className="col-md-12" onClick={handleClick} />
+							 <Button text="SignIn" type="submit" className="col-md-12" />
 						</form>
 						<p className="sgntrm145">Or <a href="forgot_password.html">Forgot Password</a>.</p>
 						
 					</div>
-					<div className="sign_footer" onClick={handleClick} > 2020 <strong>Cursus</strong>. All Rights Reserved.</div>
+					<div className="sign_footer" > 2020 <strong>Cursus</strong>. All Rights Reserved.</div>
 				</div>				
 			</div>				
 		</div>				
