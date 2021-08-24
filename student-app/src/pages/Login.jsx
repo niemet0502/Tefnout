974c6f237682fc4d7aa0c8c11153643145f22724
niemet0502 <vincentmarius8@gmail.com>
2021-08-24 17:32:37 +0200

    student :: displayed Signup form

--- student-app/src/pages/Login.jsx
+++ student-app/src/pages/Login.jsx
@@ -81,6 +81,52 @@ function Login() {
                   </form>
                 </div>
             </div>
+            <div className="col-lg-6">
+                <div className="account_wrap">
+                  <h3 className="title">Sign up your Account</h3>
+                  <form action="">
+                    <FormInput
+                      name="username"
+                      type="text"
+                      value={user.username}
+                      onChange={handleChange}
+                      placeholder="Enter username..."
+                      required
+                      error={errors.username}
+                      className="input"
+                    />
+                    <FormInput
+                      name="username"
+                      type="text"
+                      value={user.username}
+                      onChange={handleChange}
+                      placeholder="Enter email..."
+                      required
+                      error={errors.username}
+                      className="input"
+                    />
+
+                    <FormInput
+                        name="password"
+                        type="password"
+                        value={user.password}
+                        onChange={handleChange}
+                        placeholder="Enter password..."
+                        className="input"
+                        error={errors.password}
+                        required
+                      />
+
+                    <Button
+                      type="submit"
+                      className="button"
+                      text="Submit"
+                      handleClick={onSubmit}
+                    />
+
+                  </form>
+                </div>
+            </div>
           </div>
         </div>
       </section>
