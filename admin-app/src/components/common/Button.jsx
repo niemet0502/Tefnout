a56ab1168364ec7bd2277cbb6939fa0b23d701c3
niemet0502 <vincentmarius8@gmail.com>
2021-08-24 18:18:57 +0200

    admin :: added more props in Button component

--- admin-app/src/components/common/Button.jsx
+++ admin-app/src/components/common/Button.jsx
@@ -2,17 +2,37 @@ import React from 'react'
 import styled from "styled-components";
 import PropTypes from 'prop-types'; 
 
-function Button({text}) {
+function Button({
+  text, 
+  Icon,
+  bgColor,
+  bgColorHover,
+  type,
+  classNames,
+  handleClick}) {
   return (
-    <ButtonComponent>
-      {text}
+    <ButtonComponent 
+      bgColor={bgColor} 
+      bgColorHover={bgColorHover}
+      type={type}
+      className={classNames}
+      onClick={handleClick}
+      >
+        {text} 
+        {Icon && <Icon />}
     </ButtonComponent>
   )
 }
 
 Button.propTypes = {
-  text: PropTypes.string.isRequired
-}
+  text: PropTypes.string.isRequired,
+  Icon: PropTypes.node,
+  bgColor: PropTypes.string,
+  bgColorHover: PropTypes.string,
+  type: PropTypes.string,
+  classNames: PropTypes.string,
+  handleClick: PropTypes.func
+};
 
 const ButtonComponent = styled.button`
   font-size: 14px;
