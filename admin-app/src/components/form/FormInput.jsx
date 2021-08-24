77ff5ed61bc4364b1d1035ba400abced5f437b68
niemet0502 <vincentmarius8@gmail.com>
2021-08-24 18:18:29 +0200

    admin :: created FormInput component

--- admin-app/src/components/form/FormInput.jsx
+++ admin-app/src/components/form/FormInput.jsx
@@ -1,11 +1,53 @@
 import React from 'react'
+import PropTypes from 'prop-types'; 
 
-function FormInput() {
+const FormInput = ({
+  name,
+  type,
+  placeholder,
+  onChange,
+  className,
+  value,
+  error,
+  children,
+  label,
+  ...props
+}) => {
+  
   return (
-    <div>
-      
-    </div>
+    <React.Fragment>
+      <label htmlFor={name}>{label}</label>
+      <input
+        id={name}
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        onChange={onChange}
+        value={value}
+        className={className}
+        style={error && {border: 'solid 1px red'}}
+      />
+      { error && <p>{ error }</p>}
+    </React.Fragment>
   )
+}
+
+FormInput.defaultProps = {
+  type: "text",
+  className: ""
+}
+
+FormInput.propTypes = {
+  error: PropTypes.string,
+  children: PropTypes.node,
+  label: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  placeholder: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(['text', 'number', 'password']),
+  className: PropTypes.string,
+  value: PropTypes.any,
+  onChange: PropTypes.func.isRequired
 }
 
 export default FormInput
