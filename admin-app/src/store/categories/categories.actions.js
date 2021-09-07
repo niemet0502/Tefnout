42126404e94733180525d30c16bff211b62a90bd
niemet0502 <vincentmarius8@gmail.com>
2021-09-07 22:14:01 +0200

    admin :: created categories actions and the function who make call to the api

--- admin-app/src/store/categories/categories.actions.js
+++ admin-app/src/store/categories/categories.actions.js
@@ -1,1 +1,32 @@
-export const GET_CATEGORIES 
+import { getStoredAuthToken } from "../../utils/currentUser"
+
+export const GET_CATEGORIES_LOADING = 'GET CATEGORIES LOADING'
+export const GET_CATEGORIES_SUCCESS = 'GET CATEGORIES SUCCESS'
+export const GET_CATEGORIES_FAILURES = 'GET CATEGORIES FAILURES'
+
+export const getCategories = () => ({type: GET_CATEGORIES_LOADING})
+export const getCategoriesSuccess = (data) => ({type: GET_CATEGORIES_SUCCESS, payload: data}) 
+export const getCategoriesFailures = () => ({type: GET_CATEGORIES_FAILURES})
+
+export function fetchCategories(){
+  return async dispatch =>{
+    dispatch(getCategories())
+
+      try {
+        const response = await fetch('http://127.0.0.1:8000/api/categories', { 
+          method: 'get', 
+          headers: new Headers({
+            'Authorization': 'Bear '+getStoredAuthToken(), 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          })
+        });
+
+          const data = await response.json()
+          dispatch(getCategoriesSuccess(data))
+      } catch (error) {
+        console.log(error);
+        dispatch(getCategoriesFailures())
+      }
+  }
+}
