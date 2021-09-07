03000ca67b6628f8855969c2700d3c6b2bfa263e
niemet0502 <vincentmarius8@gmail.com>
2021-09-07 19:47:42 +0200

    admin :: created Categories page to display all course's categories

---
+++ admin-app/src/pages/Categories.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Categories() {
+  return (
+    <div className="wrapper">
+      <div className="container-fluid">
+        <h1>Categories</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Categories
