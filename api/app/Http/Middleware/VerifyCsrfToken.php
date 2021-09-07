541a11b0425cf624ac488f41433c451a48b750b2
niemet0502 <vincentmarius8@gmail.com>
2021-09-07 18:52:36 +0200

    api :: allowed call from axios in the api

--- api/app/Http/Middleware/VerifyCsrfToken.php
+++ api/app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        'http://127.0.0.1:8000/api/*',
+        'http://127.0.0.1:8000/api/logout'
     ];
 }
