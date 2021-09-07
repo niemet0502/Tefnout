a89c64b9d32bc1d509dcf0e20d2450c766b945f6
niemet0502 <vincentmarius8@gmail.com>
2021-09-07 18:49:37 +0200

    admin :: created routes

--- admin-app/src/App.jsx
+++ admin-app/src/App.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
 } from "react-router-dom";
-
+import PublicRoute from './components/routing/PublicRoute';
+import PrivateRoute from './components/routing/PrivateRoute';
+import { history } from './utils/history';
 // style
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
@@ -13,12 +14,19 @@ import "bootstrap/dist/js/bootstrap.min.js"
 // components
 import Dashboard from './pages/Dashboard';
 import SignIn from "./pages/SignIn"
+import ResetPassword from "./pages/ResetPassword"
 import Navigation from './components/common/Navigation';
 function App() {
   return (
     <div className="App">
-      <Navigation/>
-      <Dashboard />
+      <Router history={history}>
+        <Navigation />
+        <Switch>
+          <PrivateRoute path="/dashboard" component={Dashboard} exact />
+          <PublicRoute path="/reset-password" component={ResetPassword} exact/>
+          <PublicRoute path="/" component={SignIn}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
