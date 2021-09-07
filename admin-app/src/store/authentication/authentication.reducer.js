571cf6602a14baba8449e626353b094ff4455104
niemet0502 <vincentmarius8@gmail.com>
2021-09-07 18:51:21 +0200

    admin :: added logout action in authentication reducer

--- admin-app/src/store/authentication/authentication.reducer.js
+++ admin-app/src/store/authentication/authentication.reducer.js
@@ -2,17 +2,19 @@ import { getStoredAuthToken } from "../../utils/currentUser"
 import * as actions from "./authentication.actions"
 
 let token = getStoredAuthToken();
-const initialState = token ? { loading: true, token: token, hasErrors: false } : { loading: false, token: null, hasErrors: false };
+const initialState = token ? { loading: false, token: token, hasErrors: false } : { loading: false, token: null, hasErrors: false };
 
 
 export default function authenticationReducer(state = initialState, action){
     switch(action.type){
       case actions.USERS_LOGIN_LOADING:
-        return {loading: true, ...state}
+        return {...state, loading: true}
       case actions.USERS_LOGIN_REQUEST: 
         return {loading: false, token: action.payload, hasErrors: false}
       case actions.USERS_LOGIN_FAILURES: 
         return {...state, loading: false, hasErrors: true}
+      case actions.USERS_LOGOUT_REQUEST: 
+        return { loading: false, token: null, hasErrors: false }
       default: 
         return state
     }
