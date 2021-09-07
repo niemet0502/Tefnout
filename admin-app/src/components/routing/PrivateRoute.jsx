4c3fcc78d1d18886350e6a9cd76f1a6e098a3b01
niemet0502 <vincentmarius8@gmail.com>
2021-09-07 10:15:58 +0200

    admin :: added types for PublicRoute's props

--- admin-app/src/components/routing/PrivateRoute.jsx
+++ admin-app/src/components/routing/PrivateRoute.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import { Route, Redirect } from "react-router-dom"
 import { getStoredAuthToken } from "../../utils/currentUser"
+import PropTypes from 'prop-types'; 
 
 function PrivateRoute({component: Component, ...rest}) {
   return (
     <Route {...rest} render={props => (
       getStoredAuthToken() ?
       <Component {...props} />
-      : <Redirect to="/authenticate" />
+      : <Redirect to="/" />
     )}
     />
   )
+}
+PrivateRoute.propTypes = {
+  component: PropTypes.node,
 }
 
 export default PrivateRoute
