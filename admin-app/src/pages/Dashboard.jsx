0eb566ac5a81a6509b4da0298d5ab989c2e0874b
niemet0502 <vincentmarius8@gmail.com>
2021-09-08 19:35:02 +0200

    admin :: clean up dashboard

--- admin-app/src/pages/Dashboard.jsx
+++ admin-app/src/pages/Dashboard.jsx
@@ -6,7 +6,7 @@ import Button from '../components/common/Button'
 import Table from '../components/common/Table/DataTable'
 function Dashboard() {
   return (
-    <div className="wrapper">
+    <div className="wrap-content">
       <div className="container-fluid">
       <div className="row d-flex">
         <div className="col-md-6"><Card title="Total issue" value={15} /></div>
