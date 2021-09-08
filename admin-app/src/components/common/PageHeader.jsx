556b85100b25652fc949430fbe4d3772abce4986
niemet0502 <vincentmarius8@gmail.com>
2021-09-08 19:12:38 +0200

    admin :: created PageHeader component

--- admin-app/src/components/common/PageHeader.jsx
+++ admin-app/src/components/common/PageHeader.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import PropTypes from "prop-types"
 
-function PageHeader() {
+//components 
+import Button from "../common/Button"
+function PageHeader({Icon,text}) {
   return (
-    <div>
-      
+    <div className="card_dash1 col-md-12 d-flex align-items-center justify-content-between">
+      <div className="">
+        <h2 className="card_dash1_title">{Icon && <Icon/>}Jump Into {text} Creation</h2>
+      </div>
+      <div className="">
+        <Button text={`Create ${text}`}/>
+      </div>
     </div>
   )
 }
-
+PageHeader.propTypes = {
+  Icon: PropTypes.node.isRequired,
+  text:  PropTypes.string.isRequired
+} 
 export default PageHeader
