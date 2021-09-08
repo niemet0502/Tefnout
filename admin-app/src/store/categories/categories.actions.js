2cd0f78f50f289ffbe967655a80762d62a8bf271
niemet0502 <vincentmarius8@gmail.com>
2021-09-08 20:34:42 +0200

    admin :: created DELETE_CATEGORY action

--- admin-app/src/store/categories/categories.actions.js
+++ admin-app/src/store/categories/categories.actions.js
@@ -1,12 +1,15 @@
+import axios from "axios"
 import { getStoredAuthToken } from "../../utils/currentUser"
 
 export const GET_CATEGORIES_LOADING = 'GET CATEGORIES LOADING'
 export const GET_CATEGORIES_SUCCESS = 'GET CATEGORIES SUCCESS'
 export const GET_CATEGORIES_FAILURES = 'GET CATEGORIES FAILURES'
+export const DELETE_CATEGORY = 'DELETE CATEGORY'
 
 export const getCategories = () => ({type: GET_CATEGORIES_LOADING})
 export const getCategoriesSuccess = (data) => ({type: GET_CATEGORIES_SUCCESS, payload: data}) 
 export const getCategoriesFailures = () => ({type: GET_CATEGORIES_FAILURES})
+export const deleteCategory = (id) => ({type: DELETE_CATEGORY, payload: id})
 
 export function fetchCategories(){
   return async dispatch =>{
@@ -28,5 +31,18 @@ export function fetchCategories(){
         console.log(error);
         dispatch(getCategoriesFailures())
       }
+  }
+}
+
+export function removeCategory(id){
+  return async dispatch =>{
+    
+    try {
+      await axios.delete(`http://127.0.0.1:8000/api/categories/${id}`)
+
+      dispatch(deleteCategory(id))
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
