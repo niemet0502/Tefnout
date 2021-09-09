c1067f6d88c99fa97310062ffa346c423b7ed487
niemet0502 <vincentmarius8@gmail.com>
2021-09-09 19:57:49 +0200

    admin :: clean up Dashboard component

--- admin-app/src/pages/Dashboard.jsx
+++ admin-app/src/pages/Dashboard.jsx
@@ -3,11 +3,7 @@ import useModal from '../hooks/useModal'
 
 //components 
 import Card from '../components/common/Card'
-import Button from '../components/common/Button'
-import Table from '../components/common/Table/DataTable'
-import Modal from "../components/common/Modal"
 function Dashboard() {
-  const { isShowing: isLoginFormShowed, toggle: toggleLoginForm } = useModal();
   
   return (
     <div className="wrap-content">
@@ -15,30 +11,7 @@ function Dashboard() {
       <div className="row d-flex">
         <div className="col-md-6"><Card title="Total issue" value={15} /></div>
         <div className="col-md-6"><Card title="Total issue" value={15} /></div>
-      </div>
-
-        <button className="modal-toggle" onClick={toggleLoginForm}>
-          Login
-        </button>
-
-        <Modal
-          isShowing={isLoginFormShowed}
-          hide={toggleLoginForm}
-          title="Login"
-        >
-          <form>
-            <div className="form-group">
-              <input type="text" placeholder="Username" />
-            </div>
-            <div className="form-group">
-              <input type="text" placeholder="Password" />
-            </div>
-            <div className="form-group">
-              <input type="submit" value="Login" />
-            </div>
-          </form>
-        </Modal>
-      
+      </div>   
       </div>
     </div>
   )
