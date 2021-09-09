9f04a6410cd95d7179014fa7ace3db1f6b50189d
niemet0502 <vincentmarius8@gmail.com>
2021-09-09 14:53:22 +0200

    admin :: created Modal component

---
+++ admin-app/src/components/common/Modal.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+
+const Modal = ({ isShowing, hide, title, ...props }) =>
+  isShowing
+    ? ReactDOM.createPortal(
+        <>
+          <div className="modal-overlay">
+            <div className="modal-wrapper">
+              <div className="modal">
+                <div className="modal-header">
+                  <h4>{title}</h4>
+                  <button
+                    type="button"
+                    className="modal-close-button"
+                    onClick={hide}
+                  >
+                    <span>&times;</span>
+                  </button>
+                </div>
+                <div className="modal-body">{props.children}</div>
+              </div>
+            </div>
+          </div>
+
+          <style jsx="true">{`
+            .modal-overlay {
+              position: fixed;
+              top: 0;
+              left: 0;
+              width: 100vw;
+              height: 100vh;
+              z-index: 1040;
+              background-color: rgba(0, 0, 0, 0.2);
+            }
+
+            .modal-wrapper {
+              position: fixed;
+              top: 0;
+              left: 0;
+              z-index: 1050;
+              width: 100%;
+              height: 100%;
+              overflow-x: hidden;
+              overflow-y: auto;
+              outline: 0;
+              display: flex;
+              align-items: center;
+            }
+
+            .modal {
+              z-index: 100;
+              background: #fff;
+              position: relative;
+              margin: auto;
+              border-radius: 5px;
+              max-width: 500px;
+              width: 80%;
+              padding: 1rem;
+              display: block !important;
+              height: auto !important;
+            }
+
+            .modal-header {
+              display: flex;
+              justify-content: space-between;
+              align-items: center;
+            }
+
+            .modal-close-button {
+              font-size: 1.4rem;
+              font-weight: 700;
+              color: #000;
+              cursor: pointer;
+              border: none;
+              background: transparent;
+            }
+          `}</style>
+        </>,
+        document.body
+      )
+    : null;
+
+Modal.propTypes = {
+  isShowing: PropTypes.bool.isRequired,
+  hide: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired
+};
+
+export default Modal;
