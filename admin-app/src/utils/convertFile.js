e4c140ee2ac8b60cffa5d825fbaef2021f573f2a
niemet0502 <vincentmarius8@gmail.com>
2021-09-12 15:54:15 +0200

    admin :: created utils function to convert file into base64

---
+++ admin-app/src/utils/convertFile.js
@@ -0,0 +1,9 @@
+export function getBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+  });
+}
+
