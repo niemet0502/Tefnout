5c503c70847b8db6abb684210faa295c53ad2293
niemet0502 <vincentmarius8@gmail.com>
2021-09-12 17:03:29 +0200

    admin :: added user's information in state

--- admin-app/src/store/authentication/authentication.reducer.js
+++ admin-app/src/store/authentication/authentication.reducer.js
@@ -1,8 +1,8 @@
-import { getStoredAuthToken } from "../../utils/currentUser"
+import { getStoredAuthToken, getStoredUser } from "../../utils/currentUser"
 import * as actions from "./authentication.actions"
 
 let token = getStoredAuthToken();
-const initialState = token ? { loading: false, token: token, hasErrors: false } : { loading: false, token: null, hasErrors: false };
+const initialState = token ? { loading: false, token: token, user: getStoredUser(), hasErrors: false } : { loading: false, token: null, user: {}, hasErrors: false };
 
 
 export default function authenticationReducer(state = initialState, action){
@@ -10,11 +10,11 @@ export default function authenticationReducer(state = initialState, action){
       case actions.USERS_LOGIN_LOADING:
         return {...state, loading: true}
       case actions.USERS_LOGIN_REQUEST: 
-        return {loading: false, token: action.payload, hasErrors: false}
+        return {loading: false, token: action.payload.token, user: action.payload.user, hasErrors: false}
       case actions.USERS_LOGIN_FAILURES: 
         return {...state, loading: false, hasErrors: true}
       case actions.USERS_LOGOUT_REQUEST: 
-        return { loading: false, token: null, hasErrors: false }
+        return { loading: false, token: null, user: null, hasErrors: false }
       default: 
         return state
     }
