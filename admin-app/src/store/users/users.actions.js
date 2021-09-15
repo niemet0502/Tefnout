c2ff84fff8024f077e3f33c8d45edc1584cb34e4
niemet0502 <vincentmarius8@gmail.com>
2021-09-15 18:01:56 +0200

    admin :: created deleteUser action

--- admin-app/src/store/users/users.actions.js
+++ admin-app/src/store/users/users.actions.js
@@ -1,10 +1,13 @@
+import axios from "axios"
 export const GET_USERS_LOADING = 'GET USERS LOADING'
 export const GET_USERS_SUCCESS = 'GET USERS SUCCESS'
 export const GET_USERS_FAILURES = 'GET USERS FAILURES'
+export const DELETE_USER = 'DELETE USER'
 
 export const getUsers = () => ({type: GET_USERS_LOADING})
 export const getUsersSuccess = (users) => ({type: GET_USERS_SUCCESS, payload: users})
 export const getUsersFailures = () => ({type: GET_USERS_FAILURES})
+export const deleteUser = (user) => ({type: DELETE_USER, payload: user.id})
 
 export function fetchUsers(){
   return async dispatch => {
@@ -18,6 +21,19 @@ export function fetchUsers(){
     } catch (error) {
       console.log(error);
       dispatch(getUsersFailures())
+    }
+  }
+}
+
+export function removeUser(user){
+  return async dispatch => {
+    
+    try {
+      await axios.delete(`http://127.0.0.1:8000/api/users/${user.id}`)
+
+      dispatch(deleteUser(user))
+    } catch (error) {
+      console.log(error);
     }
   }
 }
