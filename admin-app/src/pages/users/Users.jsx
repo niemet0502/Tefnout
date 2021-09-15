b4ce015e465b4711a29c4b04b0e0992a22ff499b
niemet0502 <vincentmarius8@gmail.com>
2021-09-15 16:09:07 +0200

    admin :: created users page and display button for user creation

--- admin-app/src/pages/users/Users.jsx
+++ admin-app/src/pages/users/Users.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 
+// components 
+import PageHeader from '../../components/common/PageHeader';
+import Button from "../../components/common/Button";
+
+//icons 
+import PeopleOutlineIcon from '@material-ui/icons/PeopleOutline';
 function Users() {
   return (
-    <div>
+    <div className="wrap-content">
+      <h6 className="page-title"> <PeopleOutlineIcon /> <span>Users</span></h6>
       
+      <PageHeader 
+        Icon={PeopleOutlineIcon} 
+        text="User"
+      > 
+        <Button 
+          classNames="modal-toggle" 
+          text="Create User"
+        /> 
+      </PageHeader>
+
+      <table className="table ucp-table mt-5">
+        <thead className="thead-s">
+          <tr>
+            <th className="text-center" scope="col">Id</th>
+            <th  className="text-center" scope="col">Avatar</th>
+            <th className="text-center" scope="col">Full Name</th>
+            <th className="text-center" scope="col">Email</th>
+            <th className="text-center" scope="col">Phone</th>
+            <th className="text-center" scope="col">Action</th>
+          </tr>
+        </thead>
+      </table>
     </div>
   )
 }
