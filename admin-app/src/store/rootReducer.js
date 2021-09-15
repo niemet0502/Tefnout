806e167092c7e1560ff5c31191a3a6a99c1c6413
niemet0502 <vincentmarius8@gmail.com>
2021-09-15 17:04:52 +0200

    admin :: added usersReducer into rootReducer

--- admin-app/src/store/rootReducer.js
+++ admin-app/src/store/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import authenticationReducer from "./authentication/authentication.reducer";
 import categoriesReducer from './categories/categories.reducer';
+import usersReducer from './users/users.reducer';
 const rootReducer = combineReducers({
   authentication: authenticationReducer,
-  categories: categoriesReducer
+  categories: categoriesReducer,
+  users: usersReducer
 })
 export default rootReducer
