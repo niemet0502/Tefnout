f4e707759af835792837107e5f88e64b3b40b79c
niemet0502 <vincentmarius8@gmail.com>
2021-09-17 13:42:06 +0200

    admin :: imported dashboardReducer in rootReducer

--- admin-app/src/store/rootReducer.js
+++ admin-app/src/store/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import authenticationReducer from "./authentication/authentication.reducer";
 import categoriesReducer from './categories/categories.reducer';
+import dashboardReducer from './dashboard/dashboard.reducer';
 const rootReducer = combineReducers({
   authentication: authenticationReducer,
-  categories: categoriesReducer
+  categories: categoriesReducer,
+  dashboard: dashboardReducer
 })
 export default rootReducer
