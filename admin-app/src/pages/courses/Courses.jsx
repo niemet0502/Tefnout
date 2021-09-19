d8db12cebc574aa7067b93601a625505dede5d3d
niemet0502 <vincentmarius8@gmail.com>
2021-09-19 18:56:08 +0200

    admin :: displayed title in Courses pages

--- admin-app/src/pages/courses/Courses.jsx
+++ admin-app/src/pages/courses/Courses.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
+//icons 
+import LibraryBooksOutlinedIcon from '@material-ui/icons/LibraryBooksOutlined';
 function Courses() {
   return (
-    <div>
-      
+    <div className="wrap-content">
+      <div className="container-fluid">
+        <h6 className="page-title"> <LibraryBooksOutlinedIcon /> <span>Courses</span></h6>
+      </div>
     </div>
   )
 }
