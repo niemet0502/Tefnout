2406c45c51e3562c8a0e5ddf1cac2f9ba8c43f4d
niemet0502 <vincentmarius8@gmail.com>
2021-09-21 17:30:53 +0200

    admin :: created courses actions

---
+++ admin-app/src/store/courses/courses.actions.js
@@ -0,0 +1,37 @@
+export const GET_COURSES_LOADING = 'GET COURSES LOADGIN'
+export const GET_COURSES_SUCCESS = 'GET COURSES SUCCESS'
+export const GET_COURSES_FAILURES = 'GET COURSES FAILURES'
+
+export const getCourses = () => ({type: GET_COURSES_LOADING})
+export const getCoursesSuccess = courses => ({type: GET_COURSES_SUCCESS, payload: courses})
+export const getCoursesFailure = () => ({type: GET_COURSES_FAILURES})
+
+export function fetchCourses(){
+  return async dispatch => {
+    dispatch(getCourses())
+
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/admin/courses")
+      const data = await response.json()
+
+      dispatch(getCoursesSuccess(data.courses))
+    } catch (error) {
+      dispatch(getCoursesFailure())
+    }
+  }
+}
+
+export function fetchTeacherCourses(id){
+  return async dispatch => {
+    dispatch(getCourses())
+
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/api/teacher/${id}/courses`)
+      const data = await response.json()
+
+      dispatch(getCoursesSuccess(data.courses))
+    } catch (error) {
+      dispatch(getCoursesFailure())
+    }
+  }
+}
