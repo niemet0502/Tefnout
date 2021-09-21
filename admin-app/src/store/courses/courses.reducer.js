cbdacd795052befd2d8b892d78c3f03543a233a6
niemet0502 <vincentmarius8@gmail.com>
2021-09-21 18:03:08 +0200

    admin :: created case in reducer for course's deletion

--- admin-app/src/store/courses/courses.reducer.js
+++ admin-app/src/store/courses/courses.reducer.js
@@ -1,5 +1,5 @@
 import * as actions from "./courses.actions"
-
+import { toast } from "react-toastify"
 export const initialState = {
   loading: false,
   courses: [],
@@ -14,6 +14,13 @@ function coursesReducer(state = initialState, action){
       return {loading: false, courses: action.payload, hasErros: false}
     case actions.GET_COURSES_FAILURES:
       return {...state, hasErros: true}
+    case actions.DELETE_COURSE: 
+      toast("Course deleted...",{
+        position: toast.POSITION.BOTTOM_LEFT,
+        theme: "colored",
+        type: toast.TYPE.SUCCESS,
+      })
+        return {...state, courses: state.courses.filter(course => action.payload !== course.id)}
     default:
       return state;
   }
