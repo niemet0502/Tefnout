9bdd51bc43f8adfe62887ea92edad99ae103efe5
niemet0502 <vincentmarius8@gmail.com>
2021-09-21 18:02:48 +0200

    admin :: created action to delete courses

--- admin-app/src/store/courses/courses.actions.js
+++ admin-app/src/store/courses/courses.actions.js
@@ -1,10 +1,12 @@
 export const GET_COURSES_LOADING = 'GET COURSES LOADGIN'
 export const GET_COURSES_SUCCESS = 'GET COURSES SUCCESS'
 export const GET_COURSES_FAILURES = 'GET COURSES FAILURES'
+export const DELETE_COURSE = 'DELETE COURSE'
 
 export const getCourses = () => ({type: GET_COURSES_LOADING})
 export const getCoursesSuccess = courses => ({type: GET_COURSES_SUCCESS, payload: courses})
 export const getCoursesFailure = () => ({type: GET_COURSES_FAILURES})
+export const delCourse = id => ({type: DELETE_COURSE, payload: id})
 
 export function fetchCourses(){
   return async dispatch => {
@@ -32,6 +34,18 @@ export function fetchTeacherCourses(id){
       dispatch(getCoursesSuccess(data.courses))
     } catch (error) {
       dispatch(getCoursesFailure())
+    }
+  }
+}
+
+export function removeCourse(id){
+  return async dispatch => {
+    try {
+      await fetch(`http://127.0.0.1:8000/api/courses/${id}`)
+
+      dispatch(delCourse(id))
+    } catch (error) {
+      console.log(error);
     }
   }
 }
