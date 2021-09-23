23a2a0f24a861f6fa891ba7359626596a079d022
niemet0502 <vincentmarius8@gmail.com>
2021-09-23 14:27:54 +0200

    admin :: starRaiting component done !!

--- admin-app/src/components/review/StarRaiting.jsx
+++ admin-app/src/components/review/StarRaiting.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
-
-function StarRaiting() {
+import PropTypes from "prop-types"
+import StarRateIcon from '@mui/icons-material/StarRate';
+function StarRaiting({count}) {
   return (
-    <div>
-      
+    <div className="rating-box">
+      {[...Array(Math.round(count))].map(index => (
+        <StarRateIcon className="star-icon" key={index} style={{fill: "#f2b01e"}}/>
+      ))}
+       {[...Array(5-(Math.round(count)))].map(index => (
+        <StarRateIcon className="star-icon" key={index} />
+      ))}
     </div>
   )
 }
 
+StarRaiting.propTypes = {
+  count: PropTypes.number
+}
 export default StarRaiting
