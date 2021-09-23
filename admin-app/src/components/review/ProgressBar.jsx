596d1416cca053ab11076cf7faa0b7e0b7b1be63
niemet0502 <vincentmarius8@gmail.com>
2021-09-23 15:10:55 +0200

    admin :: created ProgressBar component

--- admin-app/src/components/review/ProgressBar.jsx
+++ admin-app/src/components/review/ProgressBar.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import PropTypes from "prop-types"
 
-function ProgressBar() {
+function ProgressBar({completed}) {
+  const containerStyles = {
+    height: 20,
+    width: '100%',
+    backgroundColor: "#e9ecef",
+    borderRadius: 5,
+  }
+
+  const fillerStyles = {
+    height: '100%',
+    width: `${completed}%`,
+    backgroundColor: `#ed2a26`,
+    borderRadius: 'inherit',
+    textAlign: 'right'
+  }
+
+  const labelStyles = {
+    padding: 5,
+    color: 'white',
+    fontWeight: 'bold'
+  }
+
   return (
-    <div>
-      
+    <div style={containerStyles}>
+      <div style={fillerStyles}>
+        <span style={labelStyles}></span>
+      </div>
     </div>
-  )
+  );
+};
+
+ProgressBar.propTypes ={
+  completed: PropTypes.number
 }
 
 export default ProgressBar
