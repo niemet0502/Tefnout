f3c37a81a8570d02b28b2ac717339d5b830a1be5
niemet0502 <vincentmarius8@gmail.com>
2021-09-24 14:23:14 +0200

    admin :: imported reviewsReducer into rootReducer

--- admin-app/src/store/rootReducer.js
+++ admin-app/src/store/rootReducer.js
@@ -4,11 +4,13 @@ import categoriesReducer from './categories/categories.reducer';
 import usersReducer from './users/users.reducer';
 import dashboardReducer from "./dashboard/dashboard.reducer";
 import coursesReducer from './courses/courses.reducer';
+import reviewsReducer from './reviews/reviews.reducer';
 const rootReducer = combineReducers({
   authentication: authenticationReducer,
   categories: categoriesReducer,
   dashboard: dashboardReducer,
   users: usersReducer,
-  courses: coursesReducer
+  courses: coursesReducer,
+  reviews: reviewsReducer
 })
 export default rootReducer
