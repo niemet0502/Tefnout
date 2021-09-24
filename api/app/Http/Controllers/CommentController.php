5f4eeb9f69fc01ee3f1498a03468592365c3cbe5
niemet0502 <vincentmarius8@gmail.com>
2021-09-24 14:18:28 +0200

    admin :: created function to get all rating and teacher rating only

--- api/app/Http/Controllers/CommentController.php
+++ api/app/Http/Controllers/CommentController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Comments;
 use Illuminate\Http\Request;
-
+use App\Models\Course;
 class CommentController extends Controller
 {
     /**
@@ -75,5 +75,40 @@ class CommentController extends Controller
     public function destroy($id)
     {
         //
+    }
+    
+    public function getTeacherComments($id){
+        $comments = Course::where('courses.teacher_id', $id)
+                ->join('follow_courses', 'follow_courses.course_id', '=', 'courses.id')
+                ->join('comments', 'comments.formation_id', '=', 'follow_courses.id')
+                ->join('users', 'users.id', '=', 'follow_courses.student_id')
+                ->select('courses.title as course_title',
+                    'comments.content as comment',
+                    'users.name as student_name',
+                    'users.firstname as student_firstname',
+                    'comments.created_at as comment_date',
+                    'users.avatar as student_avatar')
+                ->get();
+        return response([
+            'status' => 'success',
+            'comments' => $comments
+        ],200); 
+    }
+
+    public function getAllComments(){
+        $comments = Course::join('follow_courses', 'follow_courses.course_id', '=', 'courses.id')
+                ->join('comments', 'comments.formation_id', '=', 'follow_courses.id')
+                ->join('users', 'users.id', '=', 'follow_courses.student_id')
+                ->select('courses.title as course_title',
+                    'comments.content as comment',
+                    'users.name as student_name',
+                    'users.firstname as student_firstname',
+                    'comments.created_at as comment_date',
+                    'users.avatar as student_avatar')
+                ->get(); 
+        return response([
+            'status' => 'success',
+            'comments' => $comments
+        ],200);
     }
 }
