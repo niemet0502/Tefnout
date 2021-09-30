b99b0e44f258d32fe4b44e25fc1b672cc13d1042
niemet0502 <vincentmarius8@gmail.com>
2021-09-30 16:34:03 +0200

    admin :: added a new action in authentication.actions for change current user's informations

--- admin-app/src/store/authentication/authentication.actions.js
+++ admin-app/src/store/authentication/authentication.actions.js
@@ -5,11 +5,13 @@ export const USERS_LOGIN_LOADING = 'USERS LOGIN LOADING'
 export const USERS_LOGIN_REQUEST = 'LOGIN REQUEST'
 export const USERS_LOGOUT_REQUEST = 'USERS LOGOUT REQUEST'
 export const USERS_LOGIN_FAILURES = 'USERS LOGIN FAILURES'
+export const UPDATE_CURRENT_USER = 'UPDATE CURRENT USER'
 
 export const loginUser = () => ({type: USERS_LOGIN_LOADING})
 export const loginUserSuccess = user =>  ({type: USERS_LOGIN_REQUEST, payload: user})
 export const logoutUser = () => ({type: USERS_LOGOUT_REQUEST})
 export const loginUserfailure = () => ({type: USERS_LOGIN_FAILURES})
+export const fetchSettingsSuccess = user => ({type: UPDATE_CURRENT_USER, payload: user})
 
 export function login(user){
   return async dispatch =>{
