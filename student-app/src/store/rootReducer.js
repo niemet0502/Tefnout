cd2428485388e553b5e924c86e406e2493712dd0
niemet0502 <vincentmarius8@gmail.com>
2021-10-08 13:52:15 +0100

    student :: imported authentication reducer in rootReducer

--- student-app/src/store/rootReducer.js
+++ student-app/src/store/rootReducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import categoriesReducer from './categories/categories.reducer';
 import coursesReducer from './courses/courses.reducer';
 import usersReducer from './users/users.reducer';
+import authenticationReducer from "./authentication/authentication.reducer"
 const rootReducer = combineReducers({
   categories: categoriesReducer,
   users: usersReducer,
-  courses: coursesReducer
+  courses: coursesReducer,
+  authentication: authenticationReducer
 })
 
 export default rootReducer
