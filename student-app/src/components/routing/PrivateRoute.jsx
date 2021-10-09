f83b7b26b372ce3be44548332d35c19aa7057645
niemet0502 <vincentmarius8@gmail.com>
2021-10-09 21:14:01 +0100

    student :: created PrivateRoute component

---
+++ student-app/src/components/routing/PrivateRoute.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route, Redirect } from "react-router-dom"
+import { getStoredAuthToken } from "../../utils/currentUser"
+import PropTypes from 'prop-types'; 
+
+function PrivateRoute({component: Component, ...rest}) {
+  return (
+    <Route {...rest} render={props => (
+      getStoredAuthToken() ?
+      <Component {...props} />
+      : <Redirect to="/" />
+    )}
+    />
+  )
+}
+PrivateRoute.propTypes = {
+  component: PropTypes.func,
+}
+
+export default PrivateRoute
