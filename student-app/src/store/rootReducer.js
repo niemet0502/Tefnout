c6583d13a291b45a8617bc437bbc886c604ef220
niemet0502 <vincentmarius8@gmail.com>
2021-10-15 08:55:57 +0100

    student :: imported new reducers

--- student-app/src/store/rootReducer.js
+++ student-app/src/store/rootReducer.js
@@ -4,12 +4,21 @@ import coursesReducer from './courses/courses.reducer';
 import usersReducer from './users/users.reducer';
 import authenticationReducer from "./authentication/authentication.reducer"
 import trainingsReducer from "./formations/formations.reducer"
+import reviewsReducer from './reviews/reviews.reducer';
+import courseReducer from './course/course.reducer';
+import userReducer from './user/user.reducer';
+import curriculumReducer from './curriculum/curriculum.reducer';
+
 const rootReducer = combineReducers({
   categories: categoriesReducer,
   users: usersReducer,
   courses: coursesReducer,
   authentication: authenticationReducer,
-  trainings: trainingsReducer
+  trainings: trainingsReducer,
+  course: courseReducer,
+  reviews: reviewsReducer,
+  user: userReducer,
+  curriculum: curriculumReducer
 })
 
 export default rootReducer
