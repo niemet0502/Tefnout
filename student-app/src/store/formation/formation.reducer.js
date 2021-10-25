64f837017382e7383c67f242b3ec89a77a741864
niemet0502 <vincentmarius8@gmail.com>
2021-10-25 14:04:13 +0100

    student :: added currentChapter as an attribute in the store

--- student-app/src/store/formation/formation.reducer.js
+++ student-app/src/store/formation/formation.reducer.js
@@ -1,13 +1,19 @@
 import * as actions from "./formation.actions"
 
 const initialState = {
-  currentTraining: false
+  currentTraining: false,
+  trainingState : [],
+  currentChapter: {}
 }
 
 function formationReducer(state = initialState, action){
   switch (action.type) {
     case actions.CHECK_IF_TRAINING_EXIST:
       return {...state, currentTraining: action.payload}
+    case actions.GET_FORMATION_SUCCESS: 
+      return {...state, trainingState: action.payload}
+    case actions.GET_CHAPTER_SUCCESS: 
+      return {...state, currentChapter: action.payload}
     default:
       return state;
   }
