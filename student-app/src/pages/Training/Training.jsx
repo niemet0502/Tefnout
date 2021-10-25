ad288040723acf75f0ec28d2b47f3f18037743ee
niemet0502 <vincentmarius8@gmail.com>
2021-10-25 09:51:38 +0100

    student :: displayed the content of the current Chapter

--- student-app/src/pages/Training/Training.jsx
+++ student-app/src/pages/Training/Training.jsx
@@ -24,18 +24,64 @@ function Training({
     <div style={{paddingTop: '120px'}} className="border border-danger formation_wrapper">
       <div className="formation_content">
         <h2>Progression</h2>
-        <div className="col-md-12 d-flex">
+        <div className="col-md-12 d-flex  p-3">
           {trainingProgress.map(tr => (
             <span className="timeline__steps" key={tr.section_id}>
               {tr.chapters.map(chapter => (
                 <span onClick={() => setCurrentChapterId(chapter.id)} className={`timeline__step ${chapter.is_valide}`} key={chapter.id}>
-                  <span className="timeline__stepName">{chapter.chapter_title} {chapter.id}</span>
+                  <span className="timeline__stepName">{chapter.chapter_title}</span>
                   { currentChapter.id == chapter.id ? <span className="timeline__progressMarker"></span> : '' }
                 </span>
               ))}
               <span className="timeline__splitChapter"></span>
           </span>
           ))}
+        </div>
+        <div className="row mt-5">
+          <div className="col-md-8 p-3">
+
+
+            <div className="ov_text_wrap m-4">
+              <div className="ov_text">
+                  <h4>{currentChapter.title}</h4>
+                  <p>Rorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                      enim ad minim veniam, quis nostrud exercitation eyee. ullamco
+                      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                      dolorrepr ehenderit in voluptate velit esse cillum dolore eu
+                      fugiat nulla pariatur. Excepteur sint occaecat yeef cupidatat
+                      non proident, sunt in culpa qui officia deserunt mollit anim id
+                      est laborum. Sed ut perspiciatis unde omnis iste natus error sit
+                      voluptatem</p>
+              </div>
+              <div className="ov_text">
+                  <p>Rorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                      enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                      nisi ut aliquip ex ea commodo consequat.</p>
+              </div>
+              <div className="ov_text">
+                  <h4>Show Realtime Resualts :</h4>
+                  <p>Rorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                      enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                      nisiey.</p>
+              </div>
+              <div className="ov_text">
+                  <h4>Altime Support From Course Instractor :</h4>
+                  <p>Rorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                      enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                      nisi ut aliquip ex ea commodo consequat.Rorem ipsum dolor sit
+                      amet, consectetur adipisicing elit, sed do eiusmod tempor
+                      incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                      veniam, quis nostrud exercitation ullamcoey.</p>
+              </div>
+          </div>
+          </div>
+          <div className="col-md-4 border border-danger">
+            Next
+          </div>
         </div>
       </div>
     </div>
