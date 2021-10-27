e7b8c398967339c910acc8eb18c7df546e1f585b
niemet0502 <vincentmarius8@gmail.com>
2021-10-27 06:46:47 +0100

    student :: displayed media step

--- admin-app/src/pages/courses/NewCourse.jsx
+++ admin-app/src/pages/courses/NewCourse.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import AddCircleOutlineOutlinedIcon from '@material-ui/icons/AddCircleOutlineOutlined';
+import CourseInformation from "./CourseInformation";
+import CourseContent from './CourseContent';
+import CourseMedia from './CourseMedia';
+import Button from "../../components/common/Button"
 function NewCourse() {
+  const [currentStep, setCurrentStep] = useState(1)
+
+
+  const handleStep = () => {
+    switch (currentStep) {
+      case 1:
+        return <CourseInformation />
+      case 2:
+        return <CourseContent />
+      case 3: 
+        return <CourseMedia />
+      case 4: 
+       return <h1>Publier</h1>
+      default:
+        <CourseInformation />
+    }
+  }
+  
+
   return (
-    <div>
-      
+    <div className="wrap-content">
+      <div className="container-fluid">
+        <h6 className="page-title"> <AddCircleOutlineOutlinedIcon /> <span>Nouveau cours</span></h6>
+
+        <div className="steps_container step-app mt-5" id="add-course-tab">
+          <ul className="step-steps">
+            <li className={"" + (currentStep > 1 ? "done" : '') +  " " + (currentStep == 1 ? "active" : '')}>
+              <a href="#!" onClick={() => setCurrentStep(1)}>
+                <span className="number"></span>
+                <span className="step-name">Informations</span>
+              </a>
+            </li>
+            <li className={"" + (currentStep > 2 ? "done" : '') +  " " + (currentStep == 2 ? "active" : '')}>
+              <a href="#!" onClick={() => setCurrentStep(2)}>
+                <span className="number"></span>
+                <span className="step-name">Contenu</span>
+              </a>
+            </li>
+            <li className={"" + (currentStep > 3 ? "done" : '') +  " " + (currentStep == 3 ? "active" : '')}>
+              <a href="#!" onClick={() => setCurrentStep(3)}>
+                <span className="number"></span>
+                <span className="step-name">Media</span>
+              </a>
+            </li>
+            <li className={" " + (currentStep == 4 ? "active" : '')}>
+              <a href="#!" onClick={() => setCurrentStep(4)}>
+                <span className="number"></span>
+                <span className="step-name">Publier</span>
+              </a>
+            </li>
+          </ul>
+        <div className="form-step-content mt-5">
+          {handleStep()}
+        </div>    
+        <div className="step-footer step-tab-pager">
+          { currentStep > 1 ? <Button text="Precedent" handleClick={() => setCurrentStep(currentStep - 1)} /> : null}
+  
+          <Button text="Suivant" handleClick={() => setCurrentStep(currentStep + 1)} />
+        </div>   
+        </div> 
+      </div>
     </div>
   )
 }
