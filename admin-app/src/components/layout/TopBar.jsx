4c4389159888462e50b28187bdc66b829f95ae12
niemet0502 <vincentmarius8@gmail.com>
2021-10-31 19:54:09 +0100

    admin :: translate into french TopBar content

--- admin-app/src/components/layout/TopBar.jsx
+++ admin-app/src/components/layout/TopBar.jsx
@@ -45,26 +45,14 @@ function TopBar({onLogout, user}) {
 			<div className="ui search">
 			  <div className="ui left icon input swdh10">
 				
-          <input className="prompt srch10" type="text" placeholder="Search for Tuts Videos, Tutors ..."/>
 			  </div>
 			</div>
 		</div>
 		<div className="header_right">
 			<ul>
 				<li>
-					{user.profil_id == 1 ? '' : <Link to="/new-course"> <Button text="Create New Course" /> </Link>  }
+					{user.profil_id == 1 ? '' : <Link to="/new-course"> <Button text="Nouveau cours" /> </Link>  }
 				
-				</li>
-				<li>
-					<a href="shopping_cart.html" className="option_links" title="cart"><ShoppingCartOutlinedIcon /><span className="noti_count">2</span></a>
-				</li>
-				<li className="ui dropdown">
-					<a href="#" className="option_links" title="Messages"><MailOutlineOutlinedIcon /><span className="noti_count">3</span></a>
-					
-				</li>
-				<li className="ui dropdown">
-					<a href="#" className="option_links" title="Notifications"><NotificationsActiveOutlinedIcon /> <span className="noti_count">3</span></a>
-					 
 				</li>
 				<li className="ui dropdown">
 					<a href="#" className="opts_account" title="Account" onClick={toggleUserDropdown}>
@@ -86,19 +74,18 @@ function TopBar({onLogout, user}) {
 									<span>{user.email}</span>
 								</div>							
 							</div>
-							<a href="my_instructor_profile_view.html" className="dp_link_12">View { user.profil_id == 1 ? 'Administrator' : 'Teacher'} Profile</a>						
+							<a href="my_instructor_profile_view.html" className="dp_link_12">{ user.profil_id == 1 ? 'Administrateur' : 'Formateur'} Profile</a>						
 						</div>
 						<div className="night_mode_switch__btn">
 							<a href="#" id="night-mode" className="btn-night-mode">
-								<span className="icon"><NightsStayOutlinedIcon /></span> Night mode
-								<span className="btn-night-mode-switch">
-									<span className="uk-switch-button"></span>
-								</span>
 							</a>
 						</div>
-						<a href="instructor_dashboard.html" className="item channel_item">Cursus dashboard</a>						
-						
-						<button className="item channel_item col-md-12 bg-white" onClick={() => onLogout()}>Sign Out</button>
+
+						<Link to="/dashboard" className="item channel_item">
+							Dashbord
+						</Link>
+										
+						<button className="item channel_item col-md-12 bg-white" onClick={() => onLogout()}>Se deconnecter</button>
 					</div>
 					}
 
