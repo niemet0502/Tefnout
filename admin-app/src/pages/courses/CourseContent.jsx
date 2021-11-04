4118a08ca5e1855a7eff526660b46c02e8004f33
niemet0502 <vincentmarius8@gmail.com>
2021-11-04 15:32:39 +0100

    admin :: update section

--- admin-app/src/pages/courses/CourseContent.jsx
+++ admin-app/src/pages/courses/CourseContent.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageHeader from "../../components/common/PageHeader"
 import Button from "../../components/common/Button";
 import Section from "../../components/course/Section"
+import useModal from '../../hooks/useModal';
+import Modal from "../../components/common/Modal"
+import FormInput from '../../components/form/FormInput';
 const CourseContent = () => {
+  const { isShowing: isLoginFormShowed, toggle: toggleLoginForm } = useModal();
+  const [state, setState] = useState('')
+
   return (
     <div>
       <div className="title-icon">
@@ -15,10 +21,44 @@ const CourseContent = () => {
           > 
           <Button 
             text="Nouvelle Section"
+            handleClick={toggleLoginForm} 
           /> 
         </PageHeader>
+        
+        <Section />
+     
+        <Modal
+          isShowing={isLoginFormShowed}
+          hide={toggleLoginForm}
+          title="Nouvelle Section"
+        >
 
-      <Section /> 
+          <div className="ui search focus mt-2">
+            <div className="ui left icon input swdh95">
+            <FormInput
+              name="label"
+              type="text"
+              value={state}
+              onChange={(e) => setState(e.target.value)}
+              className="prompt srch_explore"
+              label="Titre"
+              required
+              />
+            </div>
+          </div>
+
+          <div className="d-flex align-items-center justify-content-end mt-4">
+             <Button 
+                classNames="modal-toggle" 
+                text="Annuler"
+                variant="secondary"
+              /> 
+              <Button
+                text="Enregistrer"
+                classNames="mr-0"
+              />
+            </div>
+        </Modal>
     </div>
   )
 }
