ce1aa9de8dc50b1c40e219bb06d36f92852b3ca9
niemet0502 <vincentmarius8@gmail.com>
2021-11-11 15:47:50 +0100

    admin :: created course reducer file

---
+++ admin-app/src/store/course/course.reducer.js
@@ -0,0 +1,12 @@
+
+const initialState = {
+  course: {}
+}
+function courseReducer(state = initialState,action){
+  switch(action.type){
+    default: 
+      return state
+  }
+}
+
+export default courseReducer
