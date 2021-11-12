e066783993feb5a7861b6dfbc2df3d0bd82cf706
niemet0502 <vincentmarius8@gmail.com>
2021-11-12 15:21:25 +0100

    admin :: added currentCourseContent as a value in the store

--- admin-app/src/store/course/course.reducer.js
+++ admin-app/src/store/course/course.reducer.js
@@ -1,9 +1,14 @@
-
+import * as actions from "./course.actions"
 const initialState = {
-  course: {}
+  currentCourse: {},
+  currentCourseContent: []
 }
 function courseReducer(state = initialState,action){
   switch(action.type){
+    case actions.CREATE_NEW_COURSE: 
+      return {...state,currentCourse: action.payload}
+    case actions.GET_CURRENT_COURSE_CONTENT: 
+      return {...state,currentCourseContent: action.payload}
     default: 
       return state
   }
