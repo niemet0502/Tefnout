adbe458db9beb332ce4218cb43679b7d51d99677
niemet0502 <vincentmarius8@gmail.com>
2021-11-17 12:46:33 +0100

    admin :: created publish course step

--- admin-app/src/pages/courses/CoursePublish.jsx
+++ admin-app/src/pages/courses/CoursePublish.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import EditIcon from '@mui/icons-material/Edit';
-const CoursePublish = () => {
+import { connect } from 'react-redux';
+import PropTypes from "prop-types"
+import { CourseContentIsValid } from '../../utils/helpers';
+const CoursePublish = ({
+  currentNewCourse
+}) => {
   return (
     <div>
       <div className="title-icon">
@@ -8,13 +13,35 @@ const CoursePublish = () => {
       </div>
       <div className="publish-block">
         <EditIcon/>
-        <p>
+
+        {CourseContentIsValid(currentNewCourse) == true ?
+          <p>
           Votre cours est à l&apos;état de brouillon. Les étudiants ne peuvent pas le voir, 
           ni s&apos;inscrire à ce cours. Le cours apparaitra quand meme dans les statistique de l&apos;Administrateur. <br />
           Si vous voulez le rendre disponible pour les etudiants vous pouvez cliquer sur le bouton publier</p>
+        : 
+        <p>
+          Le contenu de votre cours est invalide vous ne pouvez pas le publier. <br />
+          Veuillez mettre a jour le contenu du cours
+        </p>
+
+        }
+
       </div>
+
+      
     </div>
   )
 }
 
-export default CoursePublish
+CoursePublish.propTypes = {
+  currentNewCourse: PropTypes.object
+}
+
+const mapStateToProps = state => {
+  return{
+    currentNewCourse: state.course.currentCourseContent
+  }
+}
+
+export default connect(mapStateToProps)(CoursePublish)
