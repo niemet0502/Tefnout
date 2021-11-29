9409f92056f94a29f54bea8ec255a6b4b2380146
niemet0502 <vincentmarius8@gmail.com>
2021-11-29 16:05:58 +0100

    api :: created new migration to make message column nullable into applications table

---
+++ api/database/migrations/2021_11_29_150310_make_message_nullable_in_applications.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class MakeMessageNullableInApplications extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            $table->text('message')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            //
+        });
+    }
+}
