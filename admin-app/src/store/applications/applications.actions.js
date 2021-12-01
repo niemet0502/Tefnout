fa4132aa3b18459dbbac2f948dcf7f71c1f3429f
niemet0502 <vincentmarius8@gmail.com>
2021-12-01 15:06:42 +0100

    admin :: created applications reducer

---
+++ admin-app/src/store/applications/applications.actions.js
@@ -0,0 +1,22 @@
+export const GET_APPLICATIONS_LOADING = 'GET APPLICATONS LOADING'
+export const GET_APPLICATIONS_SUCCESS = 'GET APPLICATONS SUCCESS'
+export const GET_APPLICATIONS_FAILURE = 'GET APPLICATONS FAILURE'
+
+export const getApplications = () => ({type: GET_APPLICATIONS_LOADING  })
+export const getApplicationsSuccess = (applications) => ({type: GET_APPLICATIONS_SUCCESS , payload: applications})
+export const getApplicationsFailures = () => ({type: GET_APPLICATIONS_FAILURE })
+
+
+export function fetchApplications(){
+  return async dispatch => {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/applications")
+      const data = await response.json()
+
+
+      dispatch(getApplicationsSuccess(data))
+    } catch (error) {
+      
+    }
+  }
+}
